7058a67435edd91d6572afc3292a90364d92c0d1
CodenameWaddles <clovis.oudart@wanadoo.fr>
2025-10-17 11:39:57 +0200

    sign + logo on off etc

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Sign : MonoBehaviour
+{
+    
+    [SerializeField] private MeshRenderer logo;
+    [SerializeField] private Texture2D onTexture;
+    [SerializeField] private Texture2D offTexture;
+    
+    private Material _logoMaterial;
+    private bool _isOn = true;
+    
+    void Start() {
+        _logoMaterial = logo.material;
+        _logoMaterial.SetTexture("_EmissionMap", onTexture);
+    }
+    
+    public void ToggleLogo() {
+        if (_isOn) {
+            _logoMaterial.SetTexture("_EmissionMap", offTexture);
+            _isOn = false;
+        }
+        else {
+            _logoMaterial.SetTexture("_EmissionMap", onTexture);
+            _isOn = true;
+        }
+    }
+}
